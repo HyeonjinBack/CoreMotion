e083954d72fcaa979edaf1698f0881659c064dc5
HyeonjinBack <guswlsrlacl@naver.com>
2026-02-17 14:01:42 +0900

    Initial Commit

---
+++ CoreMotionWear/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  CoreMotionWear
+//
+//  Created by 백현진 on 2/17/26.
+//
+
+import SwiftUI
+import CoreMotion
+
+struct ContentView: View {
+    
+    var body: some View {
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreMotionWear/CoreMotionWearApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMotionWearApp.swift
+//  CoreMotionWear
+//
+//  Created by 백현진 on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct CoreMotionWearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
